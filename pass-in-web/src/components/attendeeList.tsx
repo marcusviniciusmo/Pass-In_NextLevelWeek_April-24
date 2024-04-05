43929822fc2e9750182eefcadf06280526e06df0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-05 17:04:20 +0100

    Data generated by Faker JS

--- pass-in-web/src/components/attendeeList.tsx
+++ pass-in-web/src/components/attendeeList.tsx
@@ -3,6 +3,7 @@ import { Table } from './table/table';
 import { TableHeader } from './table/tableHeader';
 import { TableRow } from './table/tableRow';
 import { TableCell } from './table/tableCell';
+import { attendees } from '../data/attendees';
 import {
   Search,
   MoreHorizontal,
@@ -45,26 +46,26 @@ export function AttendeeList() {
         </thead>
 
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.map((attendee) => {
             return (
-              <TableRow key={i}>
+              <TableRow key={attendee.id}>
                 <TableCell>
                   <input
                     type="checkbox"
                     className="size-4 bg-black/20 rounded border border-white/10"
                   />
                 </TableCell>
-                <TableCell>52716</TableCell>
+                <TableCell>{attendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Diego Schell Fernandes
+                      {attendee.name}
                     </span>
-                    <span>diego@rocketseat.com.br</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 dias atrás</TableCell>
-                <TableCell>3 dias atrás</TableCell>
+                <TableCell>{attendee.createdAt.toISOString()}</TableCell>
+                <TableCell>{attendee.checkedInAt.toISOString()}</TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
