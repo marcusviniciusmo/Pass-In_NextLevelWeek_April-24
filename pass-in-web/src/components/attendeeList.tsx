cf09a8dbd154b543c63959146eda35d907746c21
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-05 06:31:22 +0100

    AttendeeList component structure created

--- pass-in-web/src/components/attendeeList.tsx
+++ pass-in-web/src/components/attendeeList.tsx
@@ -1,3 +1,86 @@
+import { Search } from 'lucide-react';
+
 export function AttendeeList() {
-  return <h1>Participantes</h1>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-3">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+
+        <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            className="bg-transparent flex-1 outline-none"
+            placeholder="Buscar participante..."
+          />
+        </div>
+      </div>
+
+      <div className="border border-white/10 rounded-lg">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-white/10">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                <input type="checkbox" name="" id="" />
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Código
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Participante
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data de inscrição
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data do check-in
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left" />
+            </tr>
+          </thead>
+
+          <tbody>
+            {Array.from({ length: 8 }).map((_, i) => {
+              return (
+                <tr key={i} className="border-b border-white/10">
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <input type="checkbox" name="" id="" />
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">52716</td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">
+                        Diego Schell Fernandes
+                      </span>
+                      <span>diego@rocketseat.com.br</span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    7 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    3 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">botão</td>
+                </tr>
+              );
+            })}
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+                Mostrando 10 de 228 itens
+              </td>
+              <td
+                className="py-3 px-4 text-sm text-zinc-300 text-right"
+                colSpan={3}
+              >
+                Página 1 de 23
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 }
