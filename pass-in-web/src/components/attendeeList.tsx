366f894b54aeb059240ee3b86e0c6c5fe8390e00
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-04-06 05:43:13 +0100

    SearchInput filtering attendees

--- pass-in-web/src/components/attendeeList.tsx
+++ pass-in-web/src/components/attendeeList.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { IconButton } from './iconButton';
 import { Table } from './table/table';
 import { TableHeader } from './table/tableHeader';
 import { TableRow } from './table/tableRow';
 import { TableCell } from './table/tableCell';
-import { attendeesFakerList } from '../data/attendees';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 import {
@@ -16,25 +15,45 @@ import {
   ChevronsRight,
 } from 'lucide-react';
 
+interface Attendee {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  checkedInAt: string | null;
+}
+
 export function AttendeeList() {
   const [page, setPage] = useState(1);
-  const [attendees, setAttendees] = useState(attendeesFakerList);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
+  const [total, setTotal] = useState(0);
+  const [searchInput, setSearchInput] = useState('');
 
   const attendeePerPage = 10;
-  const totalPages = Math.ceil(attendees.length / attendeePerPage);
+  const totalPages = Math.ceil(total / attendeePerPage);
   const eventId = '9e9bd979-9d10-4915-b339-3786b1634f33';
 
   useEffect(() => {
-    fetch(`http://localhost:3333/events/${eventId}/attendees`)
+    const url = new URL(`http://localhost:3333/events/${eventId}/attendees`);
+
+    url.searchParams.set('pageIndex', String(page - 1));
+
+    if (searchInput.length > 0) {
+      url.searchParams.set('query', searchInput);
+    }
+
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        if (data) {
-          setAttendees(data.attendees);
-        } else {
-          setAttendees(attendeesFakerList);
-        }
+        setAttendees(data.attendees);
+        setTotal(data.total);
       });
-  }, [page]);
+  }, [page, searchInput]);
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setSearchInput(event.target.value);
+    setPage(1);
+  }
 
   function goToFirstPage() {
     setPage(1);
@@ -60,8 +79,10 @@ export function AttendeeList() {
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input
-            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
             placeholder="Buscar participante..."
+            value={searchInput}
+            onChange={onSearchInputChanged}
           />
         </div>
       </div>
@@ -84,56 +105,54 @@ export function AttendeeList() {
         </thead>
 
         <tbody>
-          {attendees
-            .slice((page - 1) * attendeePerPage, page * attendeePerPage)
-            .map((attendee) => {
-              return (
-                <TableRow key={attendee.id}>
-                  <TableCell>
-                    <input
-                      type="checkbox"
-                      className="size-4 bg-black/20 rounded border border-white/10"
-                    />
-                  </TableCell>
-                  <TableCell>{attendee.id}</TableCell>
-                  <TableCell>
-                    <div className="flex flex-col gap-1">
-                      <span className="font-semibold text-white">
-                        {attendee.name}
-                      </span>
-                      <span>{attendee.email}</span>
-                    </div>
-                  </TableCell>
-                  <TableCell>
-                    {attendees.createdAt
-                      ? formatDistanceToNow(attendee.createdAt, {
-                          locale: ptBR,
-                          addSuffix: true,
-                        })
-                      : 'Data de criação não disponível'}
-                  </TableCell>
-                  <TableCell>
-                    {attendee.checkedInAt
-                      ? formatDistanceToNow(attendee.checkedInAt, {
-                          locale: ptBR,
-                          addSuffix: true,
-                        })
-                      : 'Data de check-in não disponível'}
-                  </TableCell>
-                  <TableCell>
-                    <IconButton transparent>
-                      <MoreHorizontal className="size-4" />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
+          {attendees.map((attendee) => {
+            return (
+              <TableRow key={attendee.id}>
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10"
+                  />
+                </TableCell>
+                <TableCell>{attendee.id}</TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">
+                      {attendee.name}
+                    </span>
+                    <span>{attendee.email}</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {formatDistanceToNow(attendee.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true,
+                  }).toString()}
+                </TableCell>
+                <TableCell>
+                  {attendee.checkedInAt ? (
+                    formatDistanceToNow(attendee.checkedInAt, {
+                      locale: ptBR,
+                      addSuffix: true,
+                    })
+                  ) : (
+                    <span className="text-zinc-500">Não fez check-in</span>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </tbody>
 
         <tfoot>
           <tr>
             <TableCell colSpan={3}>
-              Mostrando {attendeePerPage} de {attendees.length} itens
+              Mostrando {attendees.length} de {total} itens
             </TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
